330fc1225368e2c498680e4df08e4a2a20b6d7f6
Bianca Oura <biancaoura@gmail.com>
2022-05-31 23:27:20 -0300

    Adiciona exercícios bloco 6-1

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercícios/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-e-css-forms/exercícios/script.js
@@ -1,5 +1,26 @@
 const submitButton = document.getElementById('submit-button');
 
+function formValidation() {
+  const name = document.getElementById('input-name').value.length;
+  const invalidName = name < 10 || name > 40;
+
+  const email = document.getElementById('input-email').value.length;
+  const invalidEmail = email < 10 || email > 50;
+
+  const answer = document.getElementById('type-answer').value.length;
+  const invalidAnswer = answer < 1;
+  if (invalidName || invalidEmail || invalidAnswer) {
+    return false;
+  }
+  return true;
+}
+
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
+  const isValid = formValidation();
+  if (isValid) {
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  } else {
+    alert('Dados inválidos');
+  }
 });
