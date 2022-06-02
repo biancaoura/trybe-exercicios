9882df6b32ea83108c10a3a964e48ede69bdc536
Bianca Oura <biancaoura@gmail.com>
2022-06-02 00:00:52 -0300

    Adiciona exercícios bloco 6-2

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,26 +1,82 @@
-const submitButton = document.getElementById('submit-button');
+const modal = document.querySelector('.modal');
+const span = document.querySelector('.close');
 
-function formValidation() {
-  const name = document.getElementById('input-name').value.length;
-  const invalidName = name < 10 || name > 40;
-
-  const email = document.getElementById('input-email').value.length;
-  const invalidEmail = email < 10 || email > 50;
-
-  const answer = document.getElementById('type-answer').value.length;
-  const invalidAnswer = answer < 1;
-  if (invalidName || invalidEmail || invalidAnswer) {
-    return false;
-  }
-  return true;
-}
-
-submitButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  const isValid = formValidation();
-  if (isValid) {
-    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
-  } else {
-    alert('Dados inválidos');
-  }
+let picker = new Pikaday({
+  field: document.getElementById('input-date'),
+  format: 'D/M/YYYY',
+  toString(date) {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  },
+  parse(dateString) {
+    const parts = dateString.split('/');
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1;
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  },
 });
+
+const validation = new JustValidate('#input-container');
+validation
+  .addField('#input-name', [
+    {
+      rule: 'required',
+      errorMessage: 'Nome é obrigatório',
+    },
+    {
+      rule: 'minLength',
+      value: 10,
+      errorMessage: 'Este campo precisa ter pelo menos 10 caracteres',
+    },
+    {
+      rule: 'maxLength',
+      value: 40,
+      errorMessage: 'Este campo precisa ter no máximo 40 caracteres',
+    },
+  ])
+  .addField('#input-email', [
+    {
+      rule: 'required',
+      errorMessage: 'Email é obrigatório',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email inválido!',
+    },
+    {
+      rule: 'minLength',
+      value: 10,
+      errorMessage: 'Este campo precisa ter pelo menos 10 caracteres',
+    },
+    {
+      rule: 'maxLength',
+      value: 50,
+      errorMessage: 'Este campo precisa ter no máximo 50 caracteres',
+    },
+  ])
+  .addField('#type-answer', [
+    {
+      rule: 'required',
+      errorMessage: 'Resposta é obrigatória',
+    },
+  ])
+  .addField('#agree2', [
+    {
+      rule: 'required',
+      errorMessage: 'Você deve aceitar o uso de imagem para participar',
+    },
+  ])
+  .onSuccess(() => {
+    modal.style.display = 'block';
+  });
+
+span.onclick = () => { modal.style.display = 'none'; };
+
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+};
