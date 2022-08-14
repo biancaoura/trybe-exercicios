e075516eb0f34641a22823b61e2a7f1d2e299895
Bianca Oura <biancaoura@gmail.com>
2022-08-14 16:09:17 -0300

    add ex. 3

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exe-rtl/src/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exe-rtl/src/service.test.js
@@ -1,6 +1,6 @@
 const service = require('./service');
 
-test('if function returns random int between 0 - 100', () => {  
+test('mocking function to return random int between 0 - 100', () => {  
   service.randomNumber = jest.fn().mockReturnValue(10);
 
   expect(service.randomNumber()).toBe(10)
@@ -8,7 +8,7 @@ test('if function returns random int between 0 - 100', () => {
   expect(service.randomNumber).toHaveBeenCalledTimes(1);
 });
 
-test('if function returns division of first number to second number', () => {
+test('mocking function to return division of first number to second number', () => {
   service.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
 
   expect(service.randomNumber(8, 2)).toBe(4);
@@ -17,3 +17,23 @@ test('if function returns division of first number to second number', () => {
   expect(service.randomNumber).toHaveBeenCalledWith(8, 2);
 });
 
+test('mocking function to multiply three given numbers', () => {
+  service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+  expect(service.randomNumber(2, 3, 4)).toBe(24);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  expect(service.randomNumber).toHaveBeenCalledWith(2, 3, 4);
+});
+
+test('mocking function to return the double value of argument', () => {
+  service.randomNumber.mockReset();
+  expect(service.randomNumber).toHaveBeenCalledTimes(0);
+
+  service.randomNumber = jest.fn().mockImplementationOnce((n) => n * 2);
+
+  expect(service.randomNumber(5)).toBe(10);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  expect(service.randomNumber).toHaveBeenCalledWith(5);
+});
