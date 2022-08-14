c075e16869a060c96f1a7d70a4b1fd33075cf9ba
Bianca Oura <biancaoura@gmail.com>
2022-08-14 15:58:34 -0300

    add ex. 2

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exe-rtl/src/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-02-rtl-mocks-e-inputs/exe-rtl/src/service.test.js
@@ -7,3 +7,13 @@ test('if function returns random int between 0 - 100', () => {
   expect(service.randomNumber).toHaveBeenCalled();
   expect(service.randomNumber).toHaveBeenCalledTimes(1);
 });
+
+test('if function returns division of first number to second number', () => {
+  service.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
+
+  expect(service.randomNumber(8, 2)).toBe(4);
+  expect(service.randomNumber).toHaveBeenCalled();
+  expect(service.randomNumber).toHaveBeenCalledTimes(1);
+  expect(service.randomNumber).toHaveBeenCalledWith(8, 2);
+});
+
