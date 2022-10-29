ccc0eb1daa1805a00a7d01d364397f9aaabad963
Bianca Oura <biancaoura@gmail.com>
2022-10-29 00:25:57 -0300

    cria func p remover ids 6 e 10

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-node-js-runtime-assincrono/simpsons/simpsons.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-node-js-runtime-assincrono/simpsons/simpsons.js
@@ -1,13 +1,13 @@
 const fs = require('fs').promises;
 
-// const readSimpsons = async () => {
-//   const file = await fs.readFile('./simpsons.json');
-//   const allSimpsons = JSON.parse(file);
+const readSimpsons = async () => {
+  const file = await fs.readFile('./simpsons.json');
+  const allSimpsons = JSON.parse(file);
 
-//   const simpsonsInfo = allSimpsons.map(({ id, name }) => `\n${id} - ${name}`);
+  const simpsonsInfo = allSimpsons.map(({ id, name }) => `\n${id} - ${name}`);
 
-//   console.log(...simpsonsInfo);
-// };
+  console.log(...simpsonsInfo);
+};
 
 const searchSimpsonNameById = async (id) => {
   const file = await fs.readFile('./simpsons.json');
@@ -19,9 +19,19 @@ const searchSimpsonNameById = async (id) => {
   return findSimpson;
 };
 
+const removeIds = async () => {
+  const file = await fs.readFile('./simpsons.json');
+  const simpsonsInfo = JSON.parse(file);
+
+  const modifiedArr = simpsonsInfo.filter(({ id }) => id !== '6' && id !== '10');
+
+  await fs.writeFile('./simpsons.json', JSON.stringify(modifiedArr));
+}
+
 const main = async () => {
-  // await readSimpsons();
-  console.log(await searchSimpsonNameById(2));
+  // await readSimpsons(); 
+  // console.log(await searchSimpsonNameById(2));
+  removeIds();
 };
 
 main();
