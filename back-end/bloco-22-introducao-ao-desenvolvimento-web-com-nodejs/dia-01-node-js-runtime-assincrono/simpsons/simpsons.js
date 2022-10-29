0f4c724699d8e410da1e45348a88bee93e348cf1
Bianca Oura <biancaoura@gmail.com>
2022-10-28 23:32:34 -0300

    add func p pesquisar simpson por id

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-node-js-runtime-assincrono/simpsons/simpsons.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-node-js-runtime-assincrono/simpsons/simpsons.js
@@ -1,16 +1,27 @@
 const fs = require('fs').promises;
 
-const readSimpsons = async () => {
-  const file = await fs.readFile('./simpsons.json');
-  const allSimpsons = JSON.parse(file);
+// const readSimpsons = async () => {
+//   const file = await fs.readFile('./simpsons.json');
+//   const allSimpsons = JSON.parse(file);
 
-  const simpsonsInfo = allSimpsons.map(({ id, name }) => `\n${id} - ${name}`);
-  
-  console.log(...simpsonsInfo);
+//   const simpsonsInfo = allSimpsons.map(({ id, name }) => `\n${id} - ${name}`);
+
+//   console.log(...simpsonsInfo);
+// };
+
+const searchSimpsonNameById = async (id) => {
+  const file = await fs.readFile('./simpsons.json');
+  const simpsonsInfo = JSON.parse(file);
+
+  const findSimpson = simpsonsInfo.find(simpson => Number(simpson.id) === id);
+  if (!findSimpson) throw new Error('id not found');
+
+  return findSimpson;
 };
 
 const main = async () => {
-  await readSimpsons();
+  // await readSimpsons();
+  console.log(await searchSimpsonNameById(2));
 };
 
 main();
