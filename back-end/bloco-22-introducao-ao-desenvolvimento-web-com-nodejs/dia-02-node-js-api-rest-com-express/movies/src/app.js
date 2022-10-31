451a7a4455c2eac167f027b00ffa50491a257889
Bianca Oura <biancaoura@gmail.com>
2022-10-31 18:51:22 -0300

    add ex 7

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-node-js-api-rest-com-express/movies/src/app.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-node-js-api-rest-com-express/movies/src/app.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+
 const moviesPath = path.resolve(__dirname, './movies.json');
 
 const readFile = async () => {
@@ -31,6 +33,25 @@ app.get('/movies', async (req, res) => {
     res.status(200).json(movies);
   } catch (e) {
     res.status(500).send({ message: e.message });
+  }
+});
+
+app.post('/movies', async (req, res) => {
+  try {
+    const { movie, price } = req.body;
+    const movies = await readFile();
+
+    const newMovie = {
+      id: movies[movies.length - 1].id + 1,
+      movie,
+      price,
+    };
+    const allMovies = JSON.stringify([...movies, newMovie]);
+    await fs.writeFile(moviesPath, allMovies);
+
+    res.status(201).json(newMovie);
+  } catch (e) {
+      res.status(500).send({ message: e.message });
   }
 });
 
