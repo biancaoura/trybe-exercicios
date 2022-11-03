adcf0ff3b418c7f8de926cb3c01d756325e421d1
Bianca Oura <biancaoura@gmail.com>
2022-11-03 19:49:39 -0300

    refatora middleware descrição

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-node-js-express-e-middlewares/turismo/src/middlewares/validateDescription.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-node-js-express-e-middlewares/turismo/src/middlewares/validateDescription.js
@@ -1,21 +1,17 @@
+const hasDescription = (descriptionValue, res, value) => {
+  if (!descriptionValue) {
+    return res.status(400).json({ message: `O campo ${value} é obrigatório` });
+  }
+};
+
 const validateDescription = (req, res, next) => {
   const { description } = req.body;
-  
-  if (!description) {
-    return res.status(400).json({ message: 'O campo description é obrigatório' });
-  }
-  
-  if (!description.createdAt) {
-    return res.status(400).json({ message: 'O campo createdAt é obrigatório' });
-  }
 
-  if (!description.rating) {
-    return res.status(400).json({ message: 'O campo rating é obrigatório' });
-  }
-
-  if (!description.difficulty) {
-    return res.status(400).json({ message: 'O campo difficulty é obrigatório' });
-  }
+  return hasDescription(description, res, 'description')
+    || hasDescription(description.createdAt, res, 'createdAt')
+    || hasDescription(description.rating, res, 'rating')
+    || hasDescription(description.difficulty, res, 'difficulty')
+    || next();
 };
 
 module.exports = validateDescription;
