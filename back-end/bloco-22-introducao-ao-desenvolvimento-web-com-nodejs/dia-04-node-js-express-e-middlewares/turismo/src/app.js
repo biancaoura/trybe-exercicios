0f053b83cda9ef224aabeba1a02a925f0bf800e6
Bianca Oura <biancaoura@gmail.com>
2022-11-03 19:33:09 -0300

    add middleware validação de preço

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-node-js-express-e-middlewares/turismo/src/app.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-node-js-express-e-middlewares/turismo/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const validateName = require('./middlewares/validateName');
+const validatePrice = require('./middlewares/validatePrice');
 
 app.use(express.json());
 
-app.post('/activities', validateName, (_req, res) => {
+app.post('/activities', validateName, validatePrice, (_req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
 
