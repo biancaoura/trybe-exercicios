82d45d9143614c19db9205575c24502596d708d1
Bianca Oura <biancaoura@gmail.com>
2022-11-03 19:40:46 -0300

    add middleware validação de descrição

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-node-js-express-e-middlewares/turismo/src/app.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-node-js-express-e-middlewares/turismo/src/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
+const validateDescription = require('./middlewares/validateDescription');
 
 app.use(express.json());
 
-app.post('/activities', validateName, validatePrice, (_req, res) => {
+app.post('/activities', validateName, validatePrice, validateDescription, (_req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
 
