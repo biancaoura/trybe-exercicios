1c858bf4664c6b76e163556795b5050928131f8b
Bianca Oura <biancaoura@gmail.com>
2022-11-03 20:37:52 -0300

    add token

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-node-js-express-e-middlewares/turismo/src/app.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-node-js-express-e-middlewares/turismo/src/app.js
@@ -6,6 +6,7 @@ const validateDescription = require('./middlewares/validateDescription');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
 const validateRating = require('./middlewares/validateRating');
 const validateDifficulty = require('./middlewares/validateDifficulty');
+const generateToken = require('./utils/generateToken');
 
 app.use(express.json());
 
@@ -27,7 +28,8 @@ app.post('/signup', (req, res) => {
     return res.status(401).json({ message: 'Campos ausentes!' });
   }
 
-  res.status(200).json({ message: 'ok' });
+  const token = generateToken();
+  res.status(200).json({ token });
 });
 
 module.exports = app;
