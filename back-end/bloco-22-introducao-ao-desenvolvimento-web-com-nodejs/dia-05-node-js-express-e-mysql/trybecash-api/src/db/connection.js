cc6e735d36bb4c8ae59309306d81beff66379bae
Bianca Oura <biancaoura@gmail.com>
2022-11-04 20:36:12 -0300

    add variáveis de ambiente

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-node-js-express-e-mysql/trybecash-api/src/db/connection.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-node-js-express-e-mysql/trybecash-api/src/db/connection.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'trybecashdb',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE_NAME,
+  waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTIONS,
+  connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
+  queueLimit: process.env.MYSQL_QUEUE_LIMIT,
 });
 
 module.exports = connection;
