9c10f3ecea3e5e935019c31c769bcff78644061b
Bianca Oura <biancaoura@gmail.com>
2022-11-04 17:00:14 -0300

    add rota sql

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-node-js-express-e-mysql/trybecash-api/src/routes/peopleRoutes.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-node-js-express-e-mysql/trybecash-api/src/routes/peopleRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const peopleDB = require('../db/peopleDB');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const person = req.body;
-  res.status(201).json(person);
+  try {
+    const [result] = await peopleDB.insert(person);
+    res.status(201).json({ message: `Pessoa cadastrada com sucesso com o id ${result.insertId}` });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: `Ocorreu um erro: ${e.message}`});
+  }
 });
 
 module.exports = router;
