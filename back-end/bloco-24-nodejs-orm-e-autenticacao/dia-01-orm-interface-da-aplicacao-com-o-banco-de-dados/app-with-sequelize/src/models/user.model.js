dfaf07197a1af375983b3e4a6ead9d7f03b878c6
Bianca Oura <biancaoura@gmail.com>
2022-11-22 22:13:56 -0300

    add options

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/models/user.model.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/models/user.model.js
@@ -1,7 +1,15 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
 const UserModel = (sequelize, DataTypes) => {
   const user = sequelize.define('User', {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
+  },
+  {
+    tableName: 'users',
+    underscored: true,
   });
 
   return user;
