928e5e6e52a19ec83eb51d29dc3c3918d6b92007
Bianca Oura <biancaoura@gmail.com>
2022-11-23 12:54:50 -0300

    add rotas e queries

--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/controllers/user.controller.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/controllers/user.controller.js
@@ -1,15 +1,65 @@
 const userService = require('../services/user.service');
 
+const USER_NOT_FOUND = 'Usuário não encontrado';
+
 const getAll = async (_req, res) => {
   try {
     const users = await userService.getAll();
     return res.status(200).json(users);
   } catch (e) {
     console.error(e.message);
-    res.status(500).json({ message: 'Ocorreu um erro' });
+    res.status(500).json({ message: USER_NOT_FOUND });
   }
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const user = await userService.getById(id);
+
+  if (!user) return res.status(404).json({ message: USER_NOT_FOUND });
+
+  return res.status(200).json(user);
+};
+
+const getByIdAndEmail = async (req, res) => {
+  const { id } = req.params;
+  const { email } = req.query;
+  const user = await userService.getByIdAndEmail(id, email);
+
+  if (!user) return res.status(404).json({ message: USER_NOT_FOUND });
+
+  return res.status(200).json(user);
+};
+
+const createUser = async (req, res) => {
+  const { fullName, email } = req.body;
+  const newUser = await userService.createUser(fullName, email);
+
+  return res.status(201).json(newUser);
+};
+
+const updateUser = async (req, res) => {
+  const { fullName, email } = req.body;
+  const { id } = req.params;
+  const updatedUser = await userService.updateUser(id, fullName, email);
+
+  if (!updatedUser) return res.status(404).json({ message: USER_NOT_FOUND });
+
+  return res.status(200).json({ message: 'Usuário atualizado com sucesso' });
+};
+
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  await userService.deleteUser(id);
+
+  return res.status(200).json({ message: 'Usuário excluído com sucesso' });
 };
 
 module.exports = {
   getAll,
+  getById,
+  getByIdAndEmail,
+  createUser,
+  updateUser,
+  deleteUser,
 };
--- back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/services/user.service.js
+++ back-end/bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/app-with-sequelize/src/services/user.service.js
@@ -6,6 +6,46 @@ const getAll = async () => {
   return users;
 };
 
+const getById = async (id) => {
+  const user = User.findByPk(id);
+
+  return user;
+};
+
+const getByIdAndEmail = async (id, email) => {
+  const user = await User.findOne({ where: { id, email } });
+
+  return user;
+};
+
+const createUser = async (fullName, email) => {
+  const newUser = await User.create({ fullName, email });
+
+  return newUser;
+};
+
+const updateUser = async (id, fullName, email) => {
+  const [updatedUser] = await User.update(
+    { fullName, email},
+    { where: { id } },
+  );
+
+  console.log(updatedUser);
+  return updatedUser;
+};
+
+const deleteUser = async (id) => {
+  const deletedUser = await User.destroy({ where: { id } });
+
+  console.log(deletedUser);
+  return deletedUser;
+};
+
 module.exports = {
   getAll,
+  getById,
+  getByIdAndEmail,
+  createUser,
+  updateUser,
+  deleteUser,
 };
