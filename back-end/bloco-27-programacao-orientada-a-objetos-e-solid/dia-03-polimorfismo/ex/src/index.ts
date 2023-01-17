7e830c69630a3eeaacf56bfc2f64381d6012fc9b
Bianca Oura <biancaoura@gmail.com>
2023-01-17 01:36:25 -0300

    add ex quadras

---
+++ back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-03-polimorfismo/ex/src/index.ts
@@ -0,0 +1,22 @@
+import Clube from './Clube';
+import QuadraFutebol from './QuadraFutebol';
+import QuadraTenis from './QuadraTenis';
+
+const clube = new Clube();
+const quadraFutebol = new QuadraFutebol();
+
+clube.adicionarQuadra(quadraFutebol);
+
+const quadraFut = clube.buscarQuadra<QuadraFutebol>(0);
+console.log(quadraFut);
+const reservaFut = quadraFut.reservar(new Date('2024/01/10'));
+console.log(reservaFut);
+
+const quadraTenis = new QuadraTenis();
+
+clube.adicionarQuadra(quadraTenis);
+
+const quadraTen = clube.buscarQuadra<QuadraTenis>(0);
+console.log(quadraTen);
+const reservaTen = quadraTen.reservar(new Date('2023/05/23'));
+console.log(reservaTen);
