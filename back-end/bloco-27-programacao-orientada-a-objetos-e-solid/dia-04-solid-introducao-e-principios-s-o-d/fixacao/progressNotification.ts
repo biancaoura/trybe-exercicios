2659b0a265df4341ac4fe4324db77485d3882db3
Bianca Oura <biancaoura@gmail.com>
2023-01-17 21:03:38 -0300

    add ex fixação - OCP

--- back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-04-solid-introducao-e-principios-s-o-d/fixacao/progressNotification.ts
+++ back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-04-solid-introducao-e-principios-s-o-d/fixacao/progressNotification.ts
@@ -1,19 +1,9 @@
 export default function progressNotification(
-  message: string,
   notificationType: string,
+  message: string,
 ): void {
-  switch (notificationType) {
-    case 'Email':
-      console.log('Email: ', message);
-      break;
-    case 'Phone':
-      console.log('Phone: ', message);
-      break;
-    case 'Console':
-      console.log('Console: ', message);
-      break;
-    default:
-      console.log(message);
-      break;
-  }
+      console.log(`${notificationType}: ${message}`);
 }
+
+progressNotification('Email', 'Hi');
+progressNotification('Phone', 'Call me');
