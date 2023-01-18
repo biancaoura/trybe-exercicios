fa9ba23ee1c7543b3a12d6a4be40dfe45f1d93ec
Bianca Oura <biancaoura@gmail.com>
2023-01-17 23:26:04 -0300

    código inicial

--- back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-04-solid-introducao-e-principios-s-o-d/fixacao/ReadingTracker.ts
+++ back-end/bloco-27-programacao-orientada-a-objetos-e-solid/dia-04-solid-introducao-e-principios-s-o-d/fixacao/ReadingTracker.ts
@@ -1,12 +1,15 @@
+import EmailNotification from './EmailNotification';
+import Notificator from './Notificator';
+
 type Book = {
   book: string;
   author: string;
   genre: string;
 }
 
-const progressNotification = (notification: string): void => {
-  console.log(notification);
-}
+// const progressNotification = (notification: string): void => {
+//   console.log(notification);
+// }
 
 class Wishlist {
   private wishlist: Book[];
@@ -28,8 +31,10 @@ class Wishlist {
 class ReadingTracker {
   private readingGoal: number;
   private booksRead: number;
+  notificator: Notificator;
 
-  constructor(readingGoal: number) {
+  constructor(readingGoal: number, email: string) {
+    this.notificator = new EmailNotification(email);
     this.readingGoal = readingGoal;
     this.booksRead = 0;
   }
@@ -37,20 +42,20 @@ class ReadingTracker {
   trackReadings(readsCount: number): void {
     this.booksRead += readsCount;
     if (this.booksRead >= this.readingGoal) {
-      progressNotification(
+      this.notificator.sendNotification(
         'Congratulations! You\'ve reached your reading goal!',
       );
       return;
     }
-      progressNotification(
+      this.notificator.sendNotification(
       'There are still some books to go!',
     );
   }
 }
 
-const readTracker = new ReadingTracker(20);
-const wishlist = new Wishlist({ book: 'The Road', author: 'Cormac McCarthy', genre: 'Dystopia' });
-wishlist.addToWishlist({ book: 'The Alchemist', author: 'Paulo Coelho', genre: 'Fantasy'})
-wishlist.showWishlist();
-readTracker.trackReadings(12);
-readTracker.trackReadings(9);
+// const readTracker = new ReadingTracker(20);
+// const wishlist = new Wishlist({ book: 'The Road', author: 'Cormac McCarthy', genre: 'Dystopia' });
+// wishlist.addToWishlist({ book: 'The Alchemist', author: 'Paulo Coelho', genre: 'Fantasy'})
+// wishlist.showWishlist();
+// readTracker.trackReadings(12);
+// readTracker.trackReadings(9);
