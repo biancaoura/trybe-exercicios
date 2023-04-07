b1676a2b5500b0a8a6117abc351c5ee70c1139e6
Bianca Oura <biancaoura@gmail.com>
2023-04-07 19:39:57 -0300

    add ex acompanhamento

---
+++ ciencia-da-computacao/bloco-33-padroes-de-projeto/dia-01-poo-em-python/acompanhamento/Liquidificador.py
@@ -0,0 +1,74 @@
+class Liquidificador:
+    def get_cor(self):
+        return self.__cor.upper()
+
+    def set_cor(self, nova_cor):
+        if nova_cor.lower() == "turquesa":
+            raise ValueError("Não existe liquidificador turquesa")
+        self.__cor = nova_cor
+
+    def __init__(self, cor, potencia, tensao, preco):
+        self.set_cor(cor)
+        self.preco = preco
+        self._potencia = potencia
+        self._tensao = tensao
+        self.__ligado = False
+        self.__velocidade = 0
+        self.__velocidade_maxima = 3
+        self.__corrente_atual_no_motor = 0
+
+    def ligar(self, velocidade):
+        if velocidade > self.__velocidade_maxima or velocidade < 0:
+            raise ValueError(
+                f"Velocidade deve estar entre 0 e {self.__velocidade_maxima}"
+            )
+
+        self.__velocidade = velocidade
+        self.__corrente_atual_no_motor = (
+            (self._potencia / self._tensao) / self.__velocidade_maxima
+        ) * velocidade
+        self.__ligado = True
+
+    def desligar(self):
+        self.__ligado = False
+        self.__velocidade = 0
+
+    def esta_ligado(self):
+        return self.__ligado
+
+
+class Pessoa:
+    def __init__(self, nome, saldo_na_conta):
+        self.nome = nome
+        self.saldo_na_conta = saldo_na_conta
+        self.liquidificador = None
+
+    def comprar_liquidificador(self, liquidificador):
+        if liquidificador.preco <= self.saldo_na_conta:
+            self.saldo_na_conta -= liquidificador.preco
+            self.liquidificador = liquidificador
+
+    def __str__(self):
+        return f"""
+          {self.nome}:
+          Possui {self.saldo_na_conta} reais em sua conta."""
+
+
+liquidificador_vermelho = Liquidificador("Vermelho", 250, 220, 100)
+liquidificador_vermelho.ligar(1)
+print("Está ligado?", liquidificador_vermelho.esta_ligado())
+# Está ligado? True
+liquidificador_vermelho.desligar()
+print("Está ligado?", liquidificador_vermelho.esta_ligado())
+# Está ligado? False
+
+
+liquidificador = Liquidificador("Azul", "110", "127", "200")
+print(f"A cor atual é {liquidificador.get_cor()}")
+liquidificador.set_cor("Preto")
+print(f"Após pintarmos, a nova cor é {liquidificador.get_cor()}")
+
+
+pessoa_cozinheira = Pessoa("Jacquin", 1000)
+pessoa_cozinheira.comprar_liquidificador(liquidificador_vermelho)
+print(pessoa_cozinheira)
