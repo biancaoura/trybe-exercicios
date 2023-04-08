f97ec6e11fc22c2bd1bc778ed2d50944db452fd5
Bianca Oura <biancaoura@gmail.com>
2023-04-08 00:17:43 -0300

    add ex - strategy (refactor)

--- ciencia-da-computacao/bloco-33-padroes-de-projeto/dia-02-padroes-iterator-adapter-strategy/ex/ex2.py
+++ ciencia-da-computacao/bloco-33-padroes-de-projeto/dia-02-padroes-iterator-adapter-strategy/ex/ex2.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from collections.abc import Iterator, Iterable
 
 
@@ -11,9 +12,10 @@ class Carta:
 
 
 class BaralhoIterator(Iterator):
-    def __init__(self, cartas):
+    def __init__(self, cartas, estrategia):
         self._cartas = cartas
-        self._pos = 0
+        self._estrategia = estrategia
+        self._pos = self._estrategia.posicao_inicial
 
     def __next__(self):
         try:
@@ -21,24 +23,49 @@ class BaralhoIterator(Iterator):
         except IndexError:
             raise StopIteration()
         else:
-            self._pos += 1
+            self._pos = self._estrategia.proxima_carta(self._pos)
         return carta
+
+
+class EstrategiaIteracao(ABC):
+    posicao_inicial = 0
+
+    @abstractmethod
+    def proxima_carta(cls, index):
+        raise NotImplementedError
+
+
+class EstrategiaRegular(EstrategiaIteracao):
+    posicao_inicial = 0
+
+    @classmethod
+    def proxima_carta(cls, index):
+        return index + 1
+
+
+class EstrategiaReversa(EstrategiaIteracao):
+    posicao_inicial = -1
+
+    @classmethod
+    def proxima_carta(cls, index):
+        return index - 1
 
 
 class Baralho(Iterable):
     naipes = "copas ouros espadas paus".split()
     valores = "A 2 3 4 5 6 7 8 9 10 J Q K".split()
 
-    def __init__(self):
+    def __init__(self, estrategia):
         self._cartas = [
             Carta(valor, naipe) for naipe in self.naipes for valor in self.valores
         ]
+        self.estrategia = estrategia
 
     def __len__(self):
         return len(self._cartas)
 
     def __iter__(self):
-        return BaralhoIterator(self._cartas)
+        return BaralhoIterator(self._cartas, self.estrategia)
 
     def __str__(self) -> str:
         return f"{[carta for carta in self]}"
@@ -47,29 +74,8 @@ class Baralho(Iterable):
         # return "\n".join(cartas_str)
 
 
-class BaralhoInversoIterator(Iterator):
-    def __init__(self, cartas):
-        self._cartas = cartas
-        self._pos = -1
+baralho_regular = Baralho(EstrategiaRegular())
+baralho_inverso = Baralho(EstrategiaReversa())
 
-    def __next__(self):
-        try:
-            carta = self._cartas[self._pos]
-        except IndexError:
-            raise StopIteration()
-        else:
-            self._pos -= 1
-            return carta
-
-
-class BaralhoInverso(Baralho):
-    def __iter__(self):
-        return BaralhoInversoIterator(self._cartas)
-
-
-baralho = Baralho()
-print(baralho)
-
-
-inverso = BaralhoInverso()
-print(inverso)
+print(baralho_regular)
+print(baralho_inverso)
