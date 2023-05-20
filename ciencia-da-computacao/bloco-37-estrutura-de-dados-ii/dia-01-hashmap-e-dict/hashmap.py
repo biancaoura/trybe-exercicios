a2d96c486b1396ee133f7ca0f9bf0bd533dcb39d
Bianca Oura <biancaoura@gmail.com>
2023-05-20 15:44:02 -0300

    add ex separate chaining

--- ciencia-da-computacao/bloco-37-estrutura-de-dados-ii/dia-01-hashmap-e-dict/hashmap.py
+++ ciencia-da-computacao/bloco-37-estrutura-de-dados-ii/dia-01-hashmap-e-dict/hashmap.py
@@ -6,27 +6,33 @@ class Employee:
 
 class HashMap:
     def __init__(self):
-        self._buckets = [None] * 10
+        # self._buckets = [None] * 10
+        self._buckets = [[] for i in range(10)]
 
     def get_address(self, id_num):
         return id_num % 10
 
     def insert(self, employee):
         address = self.get_address(employee.id_num)
-        self._buckets[address] = employee
+        # self._buckets[address] = employee
+        self._buckets[address].append(employee)
 
     def get_value(self, id_num):
         address = self.get_address(id_num)
-        return self._buckets[address].name
+        # return self._buckets[address].name
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None
 
     def has(self, id_num):
         address = self.get_address(id_num)
         return self._buckets[address] is not None
 
-    def update_value(self, id_num, new_value):
-        address = self.get_address(id_num)
-        employee = self._buckets[address]
-        employee.name = new_value
+    # def update_value(self, id_num, new_value):
+    #     address = self.get_address(id_num)
+    #     employee = self._buckets[address]
+    #     employee.name = new_value
 
 
 if __name__ == "__main__":
@@ -39,5 +45,5 @@ if __name__ == "__main__":
 
     print(registry.get_value(23))
     print(registry.get_value(10))
-    registry.update_value(10, "name30")
-    print(registry.get_value(10))
+    # registry.update_value(10, "name30")
+    # print(registry.get_value(10))
